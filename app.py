4c155cef9116e641435b316157bd6ff0ea46ea7c
Patrick Skipworth <pskipworth@fastmail.com>
2024-02-21 10:51:53 +0000

    Fix some test functionality

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 # file: app.py
-from lib.artist_repository import ArtistRepository
+from lib.album import Album
 from lib.album_repository import AlbumRepository
+from lib.artist import Artist
+from lib.artist_repository import ArtistRepository
 import os
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 from lib.database_connection import get_flask_database_connection
 
 # Create a new Flask app
@@ -35,7 +37,7 @@ def get_albums():
     # We use `render_template` to send the user the file `albums/all.html`
     return render_template('albums/all.html', albums=albums)
 
-# GET /artist
+# GET /album
 # Returns the album with the supplied id as HTML
 # Try it:
 #   ; open http://localhost:5001/albums/1
@@ -46,6 +48,37 @@ def get_album(id):
     album = repository.find(id)
     # We use `render_template` to send the user the file `albums/show.html`
     return render_template('albums/show.html', album=album)
+
+# GET /books/new
+# Returns a form to create a new book
+@app.route('/albums/new', methods=['GET'])
+def get_new_album():
+    return render_template('albums/new.html')
+
+# POST /albums
+# Creates a new album
+@app.route('/albums', methods=['POST'])
+def create_album():
+    # Set up the database connection and repository
+    connection = get_flask_database_connection(app)
+    repository = AlbumRepository(connection)
+
+    # Get the fields from the request form
+    title = request.form['title']
+    release_year = request.form['release_year']
+
+    # Create an album object
+    album = Album(None, title, release_year, 1)
+
+    # Check for validity and if not valid, show the form again with errors
+    if not album.is_valid():
+        return render_template('albums/new.html', album=album, errors=album.generate_errors()), 400
+
+    # Save the book to the database
+    album = repository.create(album)
+
+    # Redirect to the book's show route to the user can see it
+    return redirect(f"/albums/{album.id}")
 
 # = Artist Routes =
 
--- tests/test_album_repository.py
+++ tests/test_album_repository.py
@@ -13,17 +13,12 @@ def test_get_all_records(db_connection):
     # Assert on the results
     assert albums == [
         Album(1, 'Doolittle', 1989, 1),
-        Album(2, 'Surfer Rosa', 1988, 1),
-        Album(3, 'Waterloo', 1974, 2),
-        Album(4, 'Super Trouper', 1980, 2),
-        Album(5, 'Bossanova', 1990, 1),
-        Album(6, 'Lover', 2019, 3),
-        Album(7, 'Folklore', 2020, 3),
-        Album(8, 'I Put a Spell on You', 1965, 4),
-        Album(9, 'Baltimore', 1978, 4),
-        Album(10, 'Here Comes the Sun', 1971, 4),
-        Album(11, 'Fodder on My Wings', 1982, 4),
-        Album(12, 'Ring Ring', 1973, 2)
+        Album(2, 'Super Trouper', 1980, 2),
+        Album(3, 'Bossanova', 1990, 1),
+        Album(4, 'Folklore', 2020, 3),
+        Album(5, 'I Put a Spell on You', 1965, 4),
+        Album(6, 'Here Comes the Sun', 1971, 4),
+        Album(7, 'Ring Ring', 1973, 2)
     ]
 
 # """
